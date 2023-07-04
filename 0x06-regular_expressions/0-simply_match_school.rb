8d1d15c931912dc0315bbb1b5b15836846d4d2ba
AkpanElizabeth <ugorjielleza@gmail.com>
2023-07-04 09:36:33 -0700

    README.md/0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-# This is a regex expression that accepts one argument and pass it to a regular expression matching method
+# This regex expression accepts one argument and pass it to a regular expression matching method
 
-
-puts ARGV[0].scan(/school/).join
+puts ARGV[0].scan(/School/).join
